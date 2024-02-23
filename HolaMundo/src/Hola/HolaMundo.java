1486637017a15e5c1188fb8dea82d3b84aba41a4
Juan <Juan@LAPTOP-SDPU9GCU>
2024-02-23 13:25:33 +0100

    Es el primer commit

---
+++ HolaMundo/src/Hola/HolaMundo.java
@@ -0,0 +1,24 @@
+package Hola;
+
+public class HolaMundo {
+
+	public static void main(String[] args) {
+		 
+		
+		int i=0;
+		
+		System.out.println("HOLA MUNDO 1 ..............");
+		System.out.println("HOLA MUNDO 2 ..............");
+		
+		
+		System.out.println("HOLA MUNDO 3 ..............");
+		
+     	while(i < 4) {
+     		System.out.println("HOLA MUNDO 4 .............."+i);     	
+     		i=i+1;
+     	}
+     	
+     	
+	}
+
+}
